1fac195ab19c611f413687e634b79afa045d2704
mohan kumar bn <mkbn02@outlook.com>
2022-03-15 10:47:55 +0000

    modified s3 file

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/mkbntech/s3-bucket/aws"
   version = "2.15.0"
 
-  bucket_prefix = "${var.prefix}-my-s3-bucket"
+  bucket_prefix = "${var.prefix}-my-s3-bucket0000011"
   acl    = "private"
 
   versioning = {
